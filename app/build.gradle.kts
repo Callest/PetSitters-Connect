1b9180d59a4b28dafbe6414fccb11f1331cd9d27
Allyson Brickzin <30304491+Callest@users.noreply.github.com>
2026-05-02 10:36:00 -0400

    Add osmdroid dependency for Android
    due to google maps flag, and my app needing maps to work properly dependency is used

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation 'org.osmdroid:osmdroid-android:6.1.20'
 }
